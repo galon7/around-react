6f56d79a0820b54fc2d424572e9aceae7a731bed
Alon G <agoykman@gmail.com>
2021-11-09 15:02:08 +0200

    finish connect api

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,28 @@
+import { api } from "../utils/api";
+import React from "react";
+
 function Main(props) {
+  const [userName, setUserName] = React.useState();
+  const [userDescription, setUserDescription] = React.useState();
+  const [userAvatar, setUserAvatar] = React.useState();
+
+  React.useEffect(() => {
+    api
+      .getUserInfo()
+      .then((data) => {
+        setUserName(data.name);
+        setUserDescription(data.about);
+        setUserAvatar(data.avatar);
+        // userID = data._id;
+      })
+      .catch((err) => console.log(`Error.....: ${err}`));
+  });
+
   return (
     <main>
       <section className="profile">
         <div>
-          <img
-            src="../images/Jacques_Cousteau.jpg"
-            alt="Jacques Cousteau"
-            className="profile__avatar"
-          />
+          <img src={userAvatar} alt="Current user" className="profile__avatar" />
           <img
             src="../images/edit-img.png"
             alt="Edit"
@@ -17,14 +32,14 @@ function Main(props) {
         </div>
         <div className="profile__info">
           <div className="profile__info-header">
-            <h1 className="profile__title">Jacques Cousteau</h1>
+            <h1 className="profile__title">{userName}</h1>
             <button
               type="button"
               className="profile__info-edit"
               onClick={props.onEditProfileClick}
             />
           </div>
-          <p className="profile__subtitle">Explorer</p>
+          <p className="profile__subtitle">{userDescription}</p>
         </div>
         <button type="button" className="profile__add-button" onClick={props.onAddPlaceClick} />
       </section>
---
+++ src/utils/api.js
@@ -0,0 +1,78 @@
+class Api {
+  constructor(options) {
+    this._headers = options.headers;
+    this._baseUrl = options.baseUrl;
+  }
+
+  _getResponseData(res) {
+    if (!res.ok) {
+      return Promise.reject(`Error: ${res.status}`);
+    }
+    return res.json();
+  }
+
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  editProfile(user) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(user),
+    }).then(this._getResponseData);
+  }
+
+  addCardApi(card) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify(card),
+    }).then(this._getResponseData);
+  }
+
+  deleteCard(photoId) {
+    return fetch(`${this._baseUrl}/cards/${photoId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  like(photoId) {
+    return fetch(`${this._baseUrl}/cards/likes/${photoId}`, {
+      method: "PUT",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  unlike(photoId) {
+    return fetch(`${this._baseUrl}/cards/likes/${photoId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    }).then(this._getResponseData);
+  }
+
+  updateAvatar(avatar) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify(avatar),
+    }).then(this._getResponseData);
+  }
+}
+
+export const api = new Api({
+  baseUrl: "https://around.nomoreparties.co/v1/group-12",
+  headers: {
+    authorization: "e0cd9749-f008-4064-bc64-61e9ac8b0f57",
+    "Content-Type": "application/json",
+  },
+});
