45621f9ba990731c83d05cdcf549b16bcc8142bb
Alon G <agoykman@gmail.com>
2021-11-17 17:14:12 +0200

    Updates

---
+++ precache-manifest.4dfa65114cc6b20a943b175a07ed028f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96125e25ebff685ece29d3d97308082a",
+    "url": "/around-react/index.html"
+  },
+  {
+    "revision": "059b2e3b560ccb38bdeb",
+    "url": "/around-react/static/css/main.e9ce04ce.chunk.css"
+  },
+  {
+    "revision": "e60ede0152882f5905c7",
+    "url": "/around-react/static/js/2.f5af7643.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/around-react/static/js/2.f5af7643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8a731c30e2d9a4a1aa",
+    "url": "/around-react/static/js/3.00a273bc.chunk.js"
+  },
+  {
+    "revision": "059b2e3b560ccb38bdeb",
+    "url": "/around-react/static/js/main.8a9d1977.chunk.js"
+  },
+  {
+    "revision": "35e5f3dca5ff58602693",
+    "url": "/around-react/static/js/runtime-main.8cd1868d.js"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/around-react/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/around-react/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/around-react/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/around-react/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/around-react/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "1dddd3241e809046b3a26df0ec8e64f1",
+    "url": "/around-react/static/media/Trash.1dddd324.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/around-react/static/media/add.bdfb8ebb.svg"
+  },
+  {
+    "revision": "58afbe73d6547d1730a443f381c68d9e",
+    "url": "/around-react/static/media/edit.58afbe73.svg"
+  },
+  {
+    "revision": "a5ea67e3f33545f454434106b75fb6d2",
+    "url": "/around-react/static/media/group-full.a5ea67e3.svg"
+  },
+  {
+    "revision": "94185013a6a56d2102a4e27feab0b94d",
+    "url": "/around-react/static/media/group.94185013.svg"
+  },
+  {
+    "revision": "3e6893cd2eab107d2d5385c921e95382",
+    "url": "/around-react/static/media/header.3e6893cd.svg"
+  }
+]);
