ded0ba8588cab99a935e5aa37114fced600f2fe8
Alon G <agoykman@gmail.com>
2021-11-17 21:05:00 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { api } from "../utils/Api";
+import { api } from "../utils/api";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 import Header from "./Header";
 import Main from "./Main";
